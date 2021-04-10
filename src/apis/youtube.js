ee2bada36e4df16a6d0484160dd138c7a297d0e7
jafdoti <jafdoti@users.noreply.github.com>
2021-04-10 15:11:08 -0500

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-const KEY = process.env.REACT_APP_API_KEY_YT;
-
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         type: 'video',
         maxResults: 5,
-        key: KEY
+        key: process.env.REACT_APP_API_KEY_YT
     }
 });
